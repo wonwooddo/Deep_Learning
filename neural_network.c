810b04a5f4b9bfba34c02acd44dfbd64344f69d6
wonwooddo <wonwooddo@lgdisplay.com>
2017-05-31 16:50:37 +0900

    0531 output format

--- neural_network.c
+++ neural_network.c
@@ -20,46 +20,45 @@
 #define MAXINPUTNO 100    /*데이터 최대 개수*/ 
 
 /*함수 프로토타입 선언*/
-double f(double u) ; /*전환 함수*/
-void initwh(double wh[HIDDENNO][INPUTNO+1]) ;
-                         /*중간층의 가중치 초기화*/
-void initwo(double wo[HIDDENNO+1]) ;/*출력층의 가중치 초기화*/
-double forward(double wh[HIDDENNO][INPUTNO+1]
-         ,double wo[HIDDENNO+1],double hi[]
-         ,double e[INPUTNO]) ; /*순방향 계산*/
-int getdata(double e[][INPUTNO]) ;/*데이터 읽어들이기*/ 
+double f(double u); /*전환 함수*/
+void initwh(double wh[HIDDENNO][INPUTNO + 1]);
+/*중간층의 가중치 초기화*/
+void initwo(double wo[HIDDENNO + 1]);/*출력층의 가중치 초기화*/
+double forward(double wh[HIDDENNO][INPUTNO + 1], double wo[HIDDENNO + 1], double hi[], double e[INPUTNO]); /*순방향 계산*/
+int getdata(double e[][INPUTNO]);/*데이터 읽어들이기*/
 
-/*******************/ 
-/*    main() 함수  */ 
-/*******************/ 
+/*******************/
+/*    main() 함수  */
+/*******************/
 int main()
 {
- double wh[HIDDENNO][INPUTNO+1] ;/*중간층의 가중치*/
- double wo[HIDDENNO+1] ;/*출력층의 가중치*/
- double e[MAXINPUTNO][INPUTNO] ;/*데이터 세트*/
- double hi[HIDDENNO+1] ;/*중간층 출력*/
- double o ;/*출력*/
- int i,j ;/*반복 제어*/
- int n_of_e ;/*데이터 개수*/
- 
- /*가중치 초기화*/
- initwh(wh) ;
- initwo(wo) ;
+	double wh[HIDDENNO][INPUTNO + 1];/*중간층의 가중치*/
+	double wo[HIDDENNO + 1];/*출력층의 가중치*/
+	double e[MAXINPUTNO][INPUTNO];/*데이터 세트*/
+	double hi[HIDDENNO + 1];/*중간층 출력*/
+	double o;/*출력*/
+	int i, j;/*반복 제어*/
+	int n_of_e;/*데이터 개수*/
 
- /*입력 데이터 읽어들이기*/
- n_of_e=getdata(e) ;
- printf("데이터 개수:%d\n",n_of_e) ;
- 
- /*계산 본체*/
- for(i=0;i<n_of_e;++i){
-  printf("%d ",i) ;
-  for(j=0;j<INPUTNO;++j)
-   printf("%lf ",e[i][j]) ;
-  o=forward(wh,wo,hi,e[i]) ;
-  printf("%lf\n",o) ;
- }
+	/*가중치 초기화*/
+	initwh(wh);
+	initwo(wo);
 
- return 0 ;
+	/*입력 데이터 읽어들이기*/
+	n_of_e = getdata(e);
+	printf("데이터 개수:%d\n", n_of_e);
+
+	/*계산 본체*/
+	for (i = 0; i < n_of_e; ++i) {
+		printf("test%d : ", i);
+		for (j = 0; j < INPUTNO; ++j)
+			printf("x%d=%.0lf ", j, e[i][j]);
+		o = forward(wh, wo, hi, e[i]);
+		printf("  out=%.0lf\n", o);
+	}
+
+
+	return 0;
 }
 
 /************************/
@@ -68,83 +67,84 @@ int main()
 /************************/
 int getdata(double e[][INPUTNO])
 {
- FILE *fp = fopen("data24.txt", "r");
- if(fp==NULL)
- {
-     printf("no input data file found\n");
-     exit(1);
- }
- int n_of_e=0 ;/*데이터 세트 개수*/
- int j=0 ;/*반복 제어용*/
+	FILE *fp = fopen("data24.txt", "r");
+	if (fp == NULL)
+	{
+		printf("file open error!\n");
+		exit(1);
+	}
+	int n_of_e = 0;/*데이터 세트 개수*/
+	int j = 0;/*반복 제어용*/
 
- /*데이터 입력*/
- while(fscanf(fp,"%lf",&e[n_of_e][j])!=EOF){
-  ++ j ;
-  if(j>=INPUTNO){/*다음 데이터*/
-   j=0 ;
-   ++n_of_e ;
-  }
- }
- fclose(fp);
- return n_of_e ;
+	/*데이터 입력*/
+	while (fscanf(fp, "%lf", &e[n_of_e][j]) != EOF) {
+		++j;
+		if (j >= INPUTNO) {/*다음 데이터*/
+			j = 0;
+			++n_of_e;
+		}
+	}
+	fclose(fp);
+	return n_of_e;
 }
+
 
 /**********************/
 /*  forward() 함수    */
 /*  순방향 계산       */
 /**********************/
-double forward(double wh[HIDDENNO][INPUTNO+1],
-	double wo[HIDDENNO+1],double hi[],
-	double e[INPUTNO])
+double forward(double wh[HIDDENNO][INPUTNO + 1],
+		double wo[HIDDENNO + 1], double hi[],
+		double e[INPUTNO])
 {
- int i,j ;/*반복 제어*/
- double u ;/*가중치를 적용한 합을 구함*/
- double o ;/*출력 계산*/
+	int i, j;/*반복 제어*/
+	double u;/*가중치를 적용한 합을 구함*/
+	double o;/*출력 계산*/
 
- /*hi 계산*/
- for(i=0;i<HIDDENNO;++i){
-  u=0 ;/*가중치를 적용한 합을 구함*/
-  for(j=0;j<INPUTNO;++j)
-   u+=e[j]*wh[i][j] ; 
-  u-=wh[i][j] ;/*역치 처리*/
-  hi[i]=f(u) ;
- }
- /*출력 o 계산*/
- o=0 ;
- for(i=0;i<HIDDENNO;++i)
-  o+=hi[i]*wo[i] ;
- o-=wo[i] ;/*역치 처리*/
- 
- return f(o) ;
-} 
+	/*hi 계산*/
+	for (i = 0; i < HIDDENNO; ++i) {
+		u = 0;/*가중치를 적용한 합을 구함*/
+		for (j = 0; j < INPUTNO; ++j)
+			u += e[j] * wh[i][j];
+		u += wh[i][j];/*역치 처리*/
+		hi[i] = f(u);
+	}
+	/*출력 o 계산*/
+	o = 0;
+	for (i = 0; i < HIDDENNO; ++i)
+		o += hi[i] * wo[i];
+	o += wo[i];/*역치 처리*/
+
+	return f(o);
+}
 
 //***********************/
 /*    initwh() 함수     */
 /*중간층의 가중치 초기화*/
 /************************/
-void initwh(double wh[HIDDENNO][INPUTNO+1])
+void initwh(double wh[HIDDENNO][INPUTNO + 1])
 {
- 
- /*가중을 상수로서 부여*/
- wh[0][0]=-2 ;
- wh[0][1]=3 ;
- wh[0][2]=-1 ;
- wh[1][0]=-2 ;
- wh[1][1]=1 ;
- wh[1][2]=0.5 ;
-} 
+
+	/*가중을 상수로서 부여*/
+	wh[0][0] = -2;
+	wh[0][1] = 3;
+	wh[0][2] = 1;
+	wh[1][0] = -2;
+	wh[1][1] = 1;
+	wh[1][2] = -0.5;
+}
 
 /************************/
 /*    initwo() 함수     */
 /*출력층의 가중치 초기화*/
 /************************/
-void initwo(double wo[HIDDENNO+1])
+void initwo(double wo[HIDDENNO + 1])
 {
- /*가중을 상수로서 부여*/
- wo[0]=-60 ;
- wo[1]=94 ;
- wo[2]=-1 ;
-} 
+	/*가중을 상수로서 부여*/
+	wo[0] = -60;
+	wo[1] = 94;
+	wo[2] = 1;
+}
 
 /*******************/
 /* f() 함수         */
@@ -152,10 +152,11 @@ void initwo(double wo[HIDDENNO+1])
 /*******************/
 double f(double u)
 {
- /*계단 함수 계산*/
- if(u>=0) return 1.0 ;
- else return 0.0 ;
- 
- /*시그모이드 함수 계산*/
-// return 1.0/(1.0+exp(-u)) ;
+	/*계단 함수 계산*/
+	if (u >= 0) return 1.0;
+	else return 0.0;
+
+	/*시그모이드 함수 계산*/
+	// return 1.0/(1.0+exp(-u)) ;
 }
+
--- neuron.c
+++ neuron.c
@@ -1,57 +1,49 @@
-/**********************************************************/
-/*                   neuron.c                             */
-/* 하나의 인공 신경 계산                                    */
-/* 적당한 가중치와 역치를 가진 인공 신경을 흉내냄              */                        
-/**********************************************************/
-
-/*Visual Studio와의 호환성 확보 */
-#define _CRT_SECURE_NO_WARNINGS
-
-/* 헤더 파일 포함*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 /*기호 상수 정의*/
-#define INPUTNO 2  /*입력 수*/ 
-#define MAXINPUTNO 100    /*데이터의 최대 개수*/ 
+#define INPUTNO 2		  //입력 수 
+#define MAXINPUTNO 100    //데이터의 최대 개수 
+
+int a;
 
 /*함수 프로토타입 선언*/
-double f(double u) ; /*전환 함수*/
-void initw(double w[INPUTNO+1]) ;
-                         /*가중치와 역치의 초기화*/
-double forward(double w[INPUTNO+1]
-         ,double e[INPUTNO]) ; /*순방향 계산*/
-int getdata(double e[][INPUTNO]) ;/*데이터 읽어들이기*/ 
+double f(double u); //전환 함수
+void initw(double w[INPUTNO + 1]);//가중치와 Bias의 초기화
+double forward(double w[INPUTNO + 1], double e[INPUTNO]); //순방향 계산
+int getdata(double e[][INPUTNO]); //데이터 읽어들이기
 
-/*******************/ 
-/*    main() 함수  */ 
-/*******************/ 
+/*******************/
+/*    main() 함수  */
+/*******************/
 int main()
 {
- double w[INPUTNO+1] ;/*가중치와 역치*/
- double e[MAXINPUTNO][INPUTNO] ;/*데이터 세트*/
- double o ;/*출력*/
- int i,j ;/*반복 제어*/
- int n_of_e ;/*데이터 개수*/
- 
- /*가중치 초기화*/
- initw(w) ;
+	double w[INPUTNO + 1]; //가중치와 Bias
+	double e[MAXINPUTNO][INPUTNO]; //데이터 세트
+	double o;//출력
+	int i, j;//반복 제어
+	int n_of_e;//데이터 개수
 
- /*입력 데이터 읽어들이기*/
- n_of_e=getdata(e) ;
- printf("데이터 개수:%d\n",n_of_e) ;
- 
- /*계산 본체*/
- for(i=0;i<n_of_e;++i){
-  printf("%d ",i) ;
-  for(j=0;j<INPUTNO;++j)
-   printf("%lf ",e[i][j]) ;
-  o=forward(w,e[i]) ;
-  printf("%lf\n",o) ;
- }
+	/*가중치 초기화*/
+	initw(w);
 
- return 0 ;
+	/*입력 데이터 읽어들이기*/
+	n_of_e = getdata(e);
+	printf("데이터 개수 : %d\n\n", n_of_e);
+
+	/*계산 본체*/
+	for (i = 0; i<n_of_e; ++i) {
+		printf("test%d : ", i);
+		for (j = 0; j < INPUTNO; ++j)
+		{
+			printf("x%d=%.0lf, ",j,  e[i][j]);
+		}
+		o = forward(w, e[i]);
+		printf("%lf\n", o);
+	}
+
+	return 0;
 }
 
 /************************/
@@ -60,58 +52,58 @@ int main()
 /************************/
 int getdata(double e[][INPUTNO])
 {
- FILE *fp = fopen("data24.txt", "r");
- if(fp==NULL)
- {
-     printf("no input data file found\n");
-     exit(1);
- }
- int n_of_e=0 ;/*데이터 세트 개수*/
- int j=0 ;/*반복 제어용*/
+	FILE *fp = fopen("data24.txt", "r");
+	if (fp == NULL)
+	{
+		printf("file open error!\n");
+		exit(1);
+	}
+	int n_of_e = 0;/*데이터 세트 개수*/
+	int j = 0;/*반복 제어용*/
 
- /*데이터 입력*/
- while(fscanf(fp,"%lf",&e[n_of_e][j])!=EOF){
-  ++ j ;
-  if(j>=INPUTNO){/*다음 데이터*/
-   j=0 ;
-   ++n_of_e ;
-  }
- }
- fclose(fp);
- return n_of_e ;
+	/*데이터 입력*/
+	while (fscanf(fp, "%lf", &e[n_of_e][j]) != EOF) {
+		++j;
+		if (j >= INPUTNO) {/*다음 데이터*/
+			j = 0;
+			++n_of_e;
+		}
+	}
+	fclose(fp);
+	return n_of_e;
 }
 
 /**********************/
 /*  forward() 함수    */
 /*  순방향 계산       */
 /**********************/
-double forward(double w[INPUTNO+1],double e[INPUTNO])
+double forward(double w[INPUTNO + 1], double e[INPUTNO])
 {
- int i ;/*반복 제어*/
- double u,o ;/*중간 계산값 u와 출력 o*/
+	int i;/*반복 제어*/
+	double u, o;/*중간 계산값 u와 출력 o*/
 
- /*계산 본체*/
- u=0 ;
- for(i=0;i<INPUTNO;++i)
-  u+=e[i]*w[i] ;
- u-=w[i] ;/*역치 처리*/
- /*출력값 계산*/
- o=f(u) ;
- return o ;
-} 
+	/*계산 본체*/
+	u = 0;
+	for (i = 0; i<INPUTNO; ++i)
+		u += e[i] * w[i];
+	u += w[i];/*Bias 처리*/
+	/*출력값 계산*/
+	o = f(u);
+	return o;
+}
 
 /**********************/
 /*    initw() 함수    */
 /*  가중치 초기화     */
 /**********************/
-void initw(double w[INPUTNO+1])
+void initw(double w[INPUTNO + 1])
 {
- /*상수를 가중치로 줌*/
- w[0]=1 ;
- w[1]=1 ;
- w[2]=1.5 ;
-// w[2]=0.5 ;
-} 
+	/*상수를 가중치로 줌*/
+	w[0] = 1;
+	w[1] = 1;
+	//w[2] = -1.5;
+	w[2]= -1 ;
+}
 
 /*******************/
 /* f() 함수        */
@@ -119,10 +111,9 @@ void initw(double w[INPUTNO+1])
 /*******************/
 double f(double u)
 {
- /*계단 함수 계산*/
- if(u>=0) return 1.0 ;
- else return 0.0 ;
- 
- /*시그모이드 함수 계산*/
-// return 1.0/(1.0+exp(-u)) ;
+	/*계단 함수 계산*/
+	if (u >= 0) return 1.0;
+	else return 0.0;
+
 }
+
